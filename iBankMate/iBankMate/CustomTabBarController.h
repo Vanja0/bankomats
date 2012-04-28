6887265b223faad8a35b566b57c22ec4b17a3472
vanja <vanja.jovicevic@gmail.com>
2012-04-28 23:34:47 +0200

    custom tab bar beggining

---
+++ iBankMate/iBankMate/CustomTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTabBarController.h
+//  iBankMate
+//
+//  Created by fwefwe fwefw on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTabBarController : UITabBarController
+
+@end
---
+++ iBankMate/iBankMate/CustomTabBarController.m
@@ -0,0 +1,13 @@
+//
+//  CustomTabBarController.m
+//  iBankMate
+//
+//  Created by fwefwe fwefw on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CustomTabBarController.h"
+
+@implementation CustomTabBarController
+
+@end
